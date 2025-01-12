979842f6fd497b9a0f5de49e79316f6a2eebdc92
Affan Khamse <khamseaffan@gmail.com>
2025-01-11 23:55:14 -0500

    Model Corrected

--- src/main/java/com/khamse/home_store/model/BillItem.java
+++ src/main/java/com/khamse/home_store/model/BillItem.java
@@ -1,9 +1,13 @@
 package com.khamse.home_store.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "bill_item")
 public class BillItem {
 
     @ManyToOne
