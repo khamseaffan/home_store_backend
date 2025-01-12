4e82d558cf9c64665a7e606e260c665485fe1c2f
Affan Khamse <khamseaffan@gmail.com>
2025-01-12 00:13:17 -0500

    BillItem corrected

--- src/main/java/com/khamse/home_store/model/BillItem.java
+++ src/main/java/com/khamse/home_store/model/BillItem.java
@@ -2,6 +2,9 @@ package com.khamse.home_store.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -9,6 +12,10 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "bill_item")
 public class BillItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @ManyToOne
     @JoinColumn(name = "product_id", nullable = false)
