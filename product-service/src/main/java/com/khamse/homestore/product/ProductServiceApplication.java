198a47b73cdde7bfdf60953747aa5e50bb02f1eb
Affan Khamse <khamseaffan@gmail.com>
2025-04-20 15:06:22 -0400

    Eureka discovery server/client configure

--- product-service/src/main/java/com/khamse/homestore/product/ProductServiceApplication.java
+++ product-service/src/main/java/com/khamse/homestore/product/ProductServiceApplication.java
@@ -3,9 +3,11 @@ package com.khamse.homestore.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.khamse.homestore.common.model")
+@EnableDiscoveryClient
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
--- store-service/src/main/java/com/khamse/homestore/store/StoreServiceApplication.java
+++ store-service/src/main/java/com/khamse/homestore/store/StoreServiceApplication.java
@@ -3,9 +3,11 @@ package com.khamse.homestore.store;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.khamse.homestore.common.model")
+@EnableDiscoveryClient
 public class StoreServiceApplication {
 
 	public static void main(String[] args) {
